e2833f77faed32d44d9beabe79d9fcebfec3a0e4
tianpb <tianpb@shanghaitech.edu.cn>
2022-07-08 17:25:27 +0800

    读取配置文件

--- initial/initial.go
+++ initial/initial.go
@@ -1,10 +1,25 @@
 package initial
 
 import (
+	"fmt"
 	"go_0/routers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-ini/ini"
 )
+
+func cfgread() (a string) {
+
+	var cfg *ini.File
+
+	cfg, err := ini.Load("conf/app.ini") //此路径为ini文件的路径
+	if err != nil {
+		fmt.Println("读取ini文件失败")
+	}
+	section := cfg.Section("Server")         //读取名字为server的区域部分
+	Port := section.Key("HttpPort").String() //选择区域内的键并指定类型
+	return Port
+}
 
 func Initialall() {
 	e := gin.Default()
@@ -12,5 +27,6 @@ func Initialall() {
 	routers.VisterRoutersInit(e)  //http://localhost:8001/vister/
 	routers.DefaultRoutersInit(e) //http://localhost:8001/   http://localhost:8001/times
 	routers.UploadRoutersInit(e)
-	e.Run(":8001")
+	ht := cfgread()
+	e.Run(ht)
 }
